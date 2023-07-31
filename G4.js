5115adfb56af1964f619a30e3ea6c625225de0c2
caetano157 <marcos.caetano.15@gmail.com>
2023-07-31 01:23:21 -0300

    att 31/07

--- G4.js
+++ G4.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-
+const bodyParser = require('body-parser')
 const venom = require('venom-bot');
 let client; // Declare the client variable at the top level to have access in the entire application
-
+port = 4000;
 venom
     .create({
-        session: 'G1' //name of session
+        session: 'G4' //name of session
     })
     .then((waClient) => {
         client = waClient; // Store the client instance in the variable
@@ -18,69 +18,62 @@ venom
         console.log("Erro inicializando o client:", error);
     });
 
-
-
-function start(number, message) {
-    return new Promise((resolve, reject) => {
-        if (!client) {
-            return reject("Client não iniciliazou. Aguarde a inicialização.");
-        }
-        const formattedNumber = number + "@c.us";
-        client
-            .sendText(formattedNumber, message)
-            .then((result) => {
-                console.log('Mensagem enviada com sucesso:', result);
-                resolve(result);
-            })
-            .catch((error) => {
-                console.error('Erro ao enviar a mensagem:', error);
-                reject(error);
-            });
-        client.onMessage((message) => {
-            if (message.body === 'status') {
-                client
-                    .sendText(message.from, "ONLINE")
-                    .then((result) => {
-                        //console.log('Result: ', result); //return object success
-                        console.log("Mensagem enviada para - Mensagem: " + message + ", Número: " + number);
-                    })
-                    .catch((erro) => {
-                        //console.error('Error when sending: ', erro); //return object error
-                        console.log("Enviando Mensagem - Mensagem: " + message + ", Número: " + number);
-                    });
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: false }))
+     
+    app.get('/', (req, res) => {
+      res.send('Servidor de envio de mensagens - C0D4R')
+    })
+     
+    app.post('/send_message', (req, res) => {
+        let data = req.body;
+        let number = req.body.number;
+        let message = req.body.message;
+        start(number, message).then((result) => {
+            res.status(200).json({ success: true, result: result });
+        })
+        .catch((error) => {
+            res.status(500).json({ success: false, error: error });
+        });
+        /* res.send('Data Received: ' + JSON.stringify(data)); */
+    })
+  
+    function start(number, message) {
+        return new Promise((resolve, reject) => {
+            if (!client) {
+                return reject("Client não iniciliazou. Aguarde a inicialização.");
             }
+            const formattedNumber = number + "@c.us";
+            client
+                .sendText(formattedNumber, message)
+                .then((result) => {
+                    console.log('Mensagem enviada com sucesso');
+                    resolve("Mensagem Enviada");
+                    //resolve(result);
+                })
+                .catch((error) => {
+                    console.error('Erro ao enviar a mensagem:', error);
+                    reject(error);
+                });
+            client.onMessage((message) => {
+                if (message.body === 'status') {
+                    client
+                        .sendText(message.from, "ONLINE")
+                        .then((result) => {
+                            //console.log('Result: ', result); //return object success
+                            console.log("Mensagem enviada para - Mensagem: " + message + ", Número: " + number);
+                        })
+                        .catch((erro) => {
+                            //console.error('Error when sending: ', erro); //return object error
+                            console.log("Enviando Mensagem - Mensagem: " + message + ", Número: " + number);
+                        });
+                }
+            });
         });
-    });
-}
+    }
+     
+    app.listen(port, () => {
+      console.log('Example app listening on port 4000!')
+    })
 
 
-/* Enviar mensagem */
-router.get('/send_message/:number/:message', function(req, res, next) {
-    const number = req.params.number;
-    const message = req.params.message;
-    console.log("Enviando Mensagem - Mensagem: " + message + ", Número: " + number);
-
-    start(number, message)
-        .then((result) => {
-            res.json({ success: true, result: result });
-        })
-        .catch((error) => {
-            res.status(500).json({ success: false, error: error });
-        });
-});
-
-app.use('/', router);
-app.listen(process.env.PORT || 4000, () => {
-    console.log('Server is running on port 4000');
-});
-
-/*
-    start(number, message)
-        .then((result) => {
-            res.json({ success: true, result: result });
-        })
-        .catch((error) => {
-            res.status(500).json({ success: false, error: error });
-        });
-
-*/
